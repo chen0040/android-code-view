9dd93d41eededdb964bf9460938a9f382ce9084e
xschen <xs0040@gmail.com>
2017-08-22 11:48:20 +0800

    source code view

---
+++ src/main/java/com/github/chen0040/androidcodeview/SourceCodeView.java
@@ -0,0 +1,14 @@
+package com.github.chen0040.androidcodeview;
+
+import android.content.Context;
+import android.webkit.WebView;
+
+/**
+ * Created by chen0 on 22/8/2017.
+ */
+
+public class SourceCodeView extends WebView {
+    public SourceCodeView(Context context) {
+        super(context);
+    }
+}
