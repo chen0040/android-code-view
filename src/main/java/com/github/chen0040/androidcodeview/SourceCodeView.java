37b18f06eb9ad3b0bb02f2c82bfd65c8b24b5ec5
xschen <xs0040@gmail.com>
2017-08-23 20:42:32 +0800

    set java code

--- src/main/java/com/github/chen0040/androidcodeview/SourceCodeView.java
+++ src/main/java/com/github/chen0040/androidcodeview/SourceCodeView.java
@@ -1,6 +1,8 @@
 package com.github.chen0040.androidcodeview;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
 import android.webkit.WebView;
 
 /**
@@ -12,5 +14,37 @@ public class SourceCodeView extends WebView {
         super(context);
     }
 
-    
+    public void setJavaCode(String content) {
+        setCode(content, "java");
+    }
+
+    public void setCode(String content, String language) {
+        this.clearCache(true);
+        this.clearHistory();
+        this.getSettings().setJavaScriptEnabled(true);
+        this.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><head>");
+        sb.append("<link rel=\"stylesheet\" href=\"file:///android_asset/default.min.css\">");
+        sb.append("<script src=\"file:///android_asset/highlight.min.js\"></script>");
+        sb.append("<script>hljs.initHighlightingOnLoad();</script>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<pre><code class=\"").append(language).append("\">");
+        sb.append(HtmlUtils.encodeHtml(content));
+        sb.append("</code></pre>");
+        sb.append("</body></html>");
+
+        String html_string = sb.toString();
+
+        this.loadDataWithBaseURL("file:///android_asset/", html_string, "text/html", "UTF-8", "");
+    }
+
+    public void enableDebugging(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != (context.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE))
+            { WebView.setWebContentsDebuggingEnabled(true); }
+        }
+    }
 }
